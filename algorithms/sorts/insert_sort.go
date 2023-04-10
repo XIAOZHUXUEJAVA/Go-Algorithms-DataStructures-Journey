544f0811fe51202bb60944d9623f30259357d5b3
XIAOZHUXUEJAVA <2393751426@qq.com>
2023-04-10 18:08:49 +0800

    insert sort

---
+++ algorithms/sorts/insert_sort.go
@@ -0,0 +1,16 @@
+package sorts
+
+func insertSort(nums []int) {
+	if len(nums) < 2 {
+		return
+	}
+	for i := 1; i < len(nums); i++ {
+		key := nums[i]
+		j := i - 1
+		for j >= 0 && nums[j] > key {
+			nums[j+1] = nums[j]
+			j--
+		}
+		nums[j+1] = key
+	}
+}
--- algorithms/sorts/sort_test.go
+++ algorithms/sorts/sort_test.go
@@ -7,26 +7,31 @@ import (
 
 func TestSort(t *testing.T) {
 	originalNums := []int{8, 1, 2, 4, 3, 7, 9, 5}
-
+	expectedNums := []int{1, 2, 3, 4, 5, 7, 8, 9}
 	t.Run("Test quickSort()", func(t *testing.T) {
 		quickSort(originalNums, 0, len(originalNums)-1)
 		sortedNums := originalNums
-		expectedNums := []int{1, 2, 3, 4, 5, 7, 8, 9}
 		if !reflect.DeepEqual(sortedNums, expectedNums) {
 			t.Errorf("sortedNums: %d, expectedNums: %d", sortedNums, expectedNums)
 		}
 	})
 	t.Run("Test mergeSort()", func(t *testing.T) {
 		sortedNums := mergeSort(originalNums)
-		expectedNums := []int{1, 2, 3, 4, 5, 7, 8, 9}
 
 		if !reflect.DeepEqual(sortedNums, expectedNums) {
 			t.Errorf("sortedNums: %d, expectedNums: %d", sortedNums, expectedNums)
 		}
 	})
 	t.Run("Test bubbleSort()", func(t *testing.T) {
-		expectedNums := []int{1, 2, 3, 4, 5, 7, 8, 9}
 		bubbleSort(originalNums)
+		sortedNums := originalNums
+		if !reflect.DeepEqual(sortedNums, expectedNums) {
+			t.Errorf("sortedNums: %d, expectedNums: %d", sortedNums, expectedNums)
+		}
+	})
+
+	t.Run("Test insertSort()", func(t *testing.T) {
+		insertSort(originalNums)
 		sortedNums := originalNums
 		if !reflect.DeepEqual(sortedNums, expectedNums) {
 			t.Errorf("sortedNums: %d, expectedNums: %d", sortedNums, expectedNums)
